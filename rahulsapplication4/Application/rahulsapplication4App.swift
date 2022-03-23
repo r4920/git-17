15d61b8509dc11a4fd003a34a8a1dae506508b0d
r4920 <hr_105@yopmail.com>
2022-03-23 11:07:52 +0000

    Code Synced by DhiWise

---
+++ rahulsapplication4/Application/rahulsapplication4App.swift
@@ -0,0 +1,16 @@
+//
+//  rahulsapplication4App.swift
+//  rahulsapplication4
+
+import SwiftUI
+
+@main
+struct rahulsapplication4App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            IncomingCallNotificationView()
+        }
+    }
+}
